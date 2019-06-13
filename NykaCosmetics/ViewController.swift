31765c4c60acb89e66004eb452b3905e7ff656ce
rupikanandamuri <rupikachowdhary09@gmail.com>
2019-06-13 10:33:29 -0400

    Initial Commit

---
+++ NykaCosmetics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NykaCosmetics
+//
+//  Created by Rupika on 2019-06-13.
+//  Copyright © 2019 Rupika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
